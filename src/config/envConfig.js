ca4521d160f559468247f8e55d508d80c2b9543f
RAHUL KUMAWAT <rahul.k@palletnow.co>
2024-06-24 10:45:04 +0530

    added produrl

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -1,23 +1,22 @@
 const EnvConfig = () => {
-    let config = {
-        env: {
-            local: {
-                baseurl: "http://localhost:5000",
-                
-            },
-            production:{
-                baseurl: "https://api.example.com",
-            }
-        }
-    }
+  let config = {
+    env: {
+      local: {
+        baseurl: "http://localhost:5000",
+      },
+      production: {
+        baseurl: process.env.PROD_URL || "https://api.example.com",
+      },
+    },
+  };
 
-    let env = process.env.NODE_ENV || 'local';
+  let env = process.env.NODE_ENV || "local";
 
-    console.log(`Environment: ${env}`);
+  console.log(`Environment: ${env}`);
 
-    return {
-        envBaseurl: config.env[env].baseurl,
-    }
-}
+  return {
+    envBaseurl: config.env[env].baseurl,
+  };
+};
 
 export default EnvConfig;

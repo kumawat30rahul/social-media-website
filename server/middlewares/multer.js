e9ad687b4de19521ea90e6ff0efa45089652c188
RAHUL KUMAWAT <rahul.k@palletnow.co>
2024-06-24 22:20:20 +0530

    changes in multer again

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,23 +1,23 @@
-// const multer = require("multer");
-// const { CloudinaryStorage } = require("multer-storage-cloudinary");
-// const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
-// // Configure Cloudinary with your credentials
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-// // Create a storage engine for Multer that uses Cloudinary
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: {
-//     folder: "uploads", // Optional: specify a folder in Cloudinary
-//     allowed_formats: ["jpg", "png"], // Optional: specify allowed formats
-//   },
-// });
+// Create a storage engine for Multer that uses Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // Optional: specify a folder in Cloudinary
+    allowed_formats: ["jpg", "png"], // Optional: specify allowed formats
+  },
+});
 
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-// module.exports = upload;
+module.exports = upload;

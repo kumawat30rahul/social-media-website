d3b7cfad53529b00a85d263922737de8e883196a
RAHUL KUMAWAT <rahul.k@palletnow.co>
2024-06-24 19:21:17 +0530

    changed multer

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/tmp/uploads");
+    cb(null, "/public");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);

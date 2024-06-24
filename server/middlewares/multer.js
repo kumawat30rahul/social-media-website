8db778a7ee64ec39f7ca3b46697f9ea3f75eb511
RAHUL KUMAWAT <rahul.k@palletnow.co>
2024-06-24 18:50:36 +0530

    change in multer

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,6 +1,17 @@
 const multer = require("multer");
+const mkdirp = require("mkdirp");
 
-const storage = multer.memoryStorage();
+const uploadDir = "/tmp/uploads";
+
+mkdirp.sync(uploadDir);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
 const upload = multer({ storage: storage });
 
